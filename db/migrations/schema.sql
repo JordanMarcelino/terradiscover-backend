fcd81417df00390f1c37252479edb49e69a8944e
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-11 17:26:42 +0700

    chore: fix schema

--- db/migrations/schema.sql
+++ db/migrations/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE IF NOT EXISTS contacts(
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(id),
     full_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL phone VARCHAR(100) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(100) NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_user_email ON users(email);
 CREATE INDEX IF NOT EXISTS idx_fk_contact_user ON contacts(user_id);
